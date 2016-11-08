6f148bbec65fa64208fc2bde6852f71dc489ba67
celia albano <albano.celia@gmail.com>
2016-11-08 15:09:22 +0100

    To do list done

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161108102224) do
+ActiveRecord::Schema.define(version: 20161108105307) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "name"
